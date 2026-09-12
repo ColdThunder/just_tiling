f2575e810c35f74353d7ab0288353e2439406250
zjdingastro <zd585612@ohio.edu>
2026-09-12 10:08:56 +0800

    add __init__.py

---
+++ py/just_tiling/__init__.py
@@ -0,0 +1,20 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+============
+just_tiling
+============
+
+This package is a template for other JUST_ Python_ packages.
+
+You should use :command:`just_update_version` to set the version.  This requires
+the justutil_ package.
+
+.. _JUST: https://just.sjtu.edu.cn/EN/about.php
+.. _Python: https://www.python.org
+.. _justutil: https://github.com/desihub/justutil
+"""
+#
+# Set version string.
+#
+from ._version import __version__  # noqa: F401
